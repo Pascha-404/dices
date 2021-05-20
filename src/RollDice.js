89f174ce098ee86e50551755e73e7e0aff7eafcf
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-20 19:24:33 +0200

    final solution

--- src/RollDice.js
+++ src/RollDice.js
@@ -5,32 +5,37 @@ import './RollDice.css'
 class RollDice extends Component{
     constructor(props){
         super(props);
-        this.state = {dice1: "one", dice2: "one"}
+        this.state = {dice1: "one", dice2: "one", btnTxt: "Roll Dice!", isRolling: false}
         this.roll = this.roll.bind(this)
-    }
-    static defaultProps = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five",6: "six"}
-
-    rndNum(e){
-        const number = Math.floor(Math.random() * 6) + 1
-        return number
-    }
+    };
+    static defaultProps = {eyes: ["one", "two", "three", "four", "five", "six"]};
 
     roll(e){
-            const die1 = this.props.rndNum();
-            const die2 = this.props.rndNum();
-            this.setState({dice1: die1, dice2: die2});
-        }
+        this.setState({btnTxt: "Rolling...", isRolling: true})
+            const num1 = Math.floor(Math.random() * 6);
+            const num2 = Math.floor(Math.random() * 6);
+            const die1 = this.props.eyes[num1];
+            const die2 = this.props.eyes[num2];
+            setTimeout(() => {
+                this.setState({btnTxt: "Roll Dice!", isRolling: false})
+                this.setState({dice1: die1, dice2: die2});
+            }, 600); 
+        };
 
     render(){
         return(
             <div className="RollDice">
                 <h1>Random Dices</h1>
-                <div className="RollDice-dices">
-                    <Die eye="one" />
-                    <Die eye="one" />
+                <div className={"RollDice-dices " + (this.state.isRolling ? "active" : null)}>
+                    <Die eye={this.state.dice1} />
+                    <Die eye={this.state.dice2} />
                 </div>
-                <button className="RollDice-btn" onClick={this.roll}>
-                    Roll Dice!
+                <button className="RollDice-btn" 
+                        onClick={this.roll} 
+                        disabled={this.state.isRolling
+                        ? true
+                        : false}>
+                    {this.state.btnTxt}
                 </button>
             </div>
         )
